1d679b9a8495da34e9c69766f8b2383affcf1aa3
Phillip DuLion <phillip@dulion.com>
2025-10-02 20:41:08 -0700

    Add some front page content

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -20,16 +20,42 @@ pub fn Home() -> impl IntoView {
             }
         }>
             <div class="navbar bg-base-200 shadow-lg">
-                <div class="flex-none">
-                <img
-                    src="images/lion.svg"
-                    alt="DuLion Logo"
-                    height="110"
-                    width="110"
-                />
+                <div class="avatar">
+                    <div class="flex-none rounded-full border-1 border-accent shadow-lg">
+                        <img src="images/lion.svg" alt="DuLion Logo" height="110" width="110" />
+                    </div>
                 </div>
                 <h1 class="flex-1 flex text-4xl justify-center">"DuLion Technology"</h1>
                 <p class="flex-none"><Button /></p>
+            </div>
+            <div class="container mx-auto flex flex-wrap justify-center items-center gap-4 mt-4 items-start">
+                <div class="card card-border bg-base-100 w-2xl shadow-lg">
+                    <div class="card-body">
+                        <h2 class="card-title">"Software Technology"</h2>
+                        <p>"My career has focused around building enterprise integration \
+                        solutions, including roles handling architecture, software engineering, \
+                        platform engineering, team leadership, and consulting. I've accumulated \
+                        more than 25-years experience and knowledge, covering a broad spectrum of \
+                        design, development, open source, and networking technologies. My primary \
+                        expertise has been in building scalable enterprise integration solutions, \
+                        distributed services, and web-based application architectures using various \
+                        open-source technologies. I have a reputation for getting projects launched \
+                        from conception, researching and evolving practical designs, quickly \
+                        adopting new technologies, and delivering solid, reliable, maintainable, \
+                        scalable solutions."</p>
+                    </div>
+                </div>
+                <div class="card card-border bg-base-100 w-2xl shadow-lg">
+                    <div class="card-body">
+                        <h2 class="card-title">"Embedded Electronics"</h2>
+                        <p>"I've always had an extensive curiosity and passion for creating \
+                        interesting solutions. As small and embedded electronics have become \
+                        more accessible, I've studied and experimented whatever I could get my \
+                        hands on. As a result, I've accumulated a significant stash of gadgets and \
+                        a staggering number of projects. I plan to write about my various projects \
+                        in the blogging section."</p>
+                    </div>
+                </div>
             </div>
         </ErrorBoundary>
     }
